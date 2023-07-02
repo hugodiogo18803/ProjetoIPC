1601dce01b76c1d1865ce400ee5cea394e730c1b
hugo <18803@stu.ipbeja.pt>
2023-07-02 17:53:30 +0100

    Medication Screen - Missing Button Actions

--- app/src/main/java/com/example/easymed/MedicationFragment.java
+++ app/src/main/java/com/example/easymed/MedicationFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+//TODO ADD BUTTON FUNCIONALITY
 
 /**
  * A simple {@link Fragment} subclass.
