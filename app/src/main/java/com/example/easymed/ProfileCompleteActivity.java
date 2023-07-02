2e25f2d1c659f3762659eb0b7fd2e145dc7191f9
hugo <18803@stu.ipbeja.pt>
2023-07-02 16:05:58 +0100

    Login Complete!!
    Time to populate the main Activity Fragments with their contents.

--- app/src/main/java/com/example/easymed/ProfileCompleteActivity.java
+++ app/src/main/java/com/example/easymed/ProfileCompleteActivity.java
@@ -7,25 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ProfileCreation2Activity extends AppCompatActivity {
+public class ProfileCompleteActivity extends AppCompatActivity {
 
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_creation2);
+        setContentView(R.layout.activity_profile_complete);
 
-        button = (Button) findViewById(R.id.doneButton);
+        button = (Button) findViewById(R.id.okButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openProfileCreationCompleteActivity();
+                openMainActivity();
             }
         });
     }
 
-    public void openProfileCreationCompleteActivity() {
-        Intent intent = new Intent(this, ProfileCompleteActivity.class);
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
