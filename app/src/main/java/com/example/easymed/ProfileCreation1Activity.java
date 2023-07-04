aa61d65121c952a1ab33f388af88998927c342ca
anton <19122@stu.ipbeja.pt>
2023-07-04 04:28:49 +0100

    Added shit on create activity 1, to make the user have to fill all fields to move onto the next activity

--- app/src/main/java/com/example/easymed/ProfileCreation1Activity.java
+++ app/src/main/java/com/example/easymed/ProfileCreation1Activity.java
@@ -6,20 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class ProfileCreation1Activity extends AppCompatActivity {
 
     private Button button;
+    private RadioGroup genderOptions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_creation1);
 
-        button = (Button) findViewById(R.id.continueButton);
+        button = findViewById(R.id.continueButton);
+        genderOptions = findViewById(R.id.gender_options);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openProfileCreation2Activity();
+                EditText firstNameEditText = findViewById(R.id.editTextText2);
+                EditText lastNameEditText = findViewById(R.id.editTextText3);
+                EditText birthDateEditText = findViewById(R.id.birth_date);
+                RadioButton maleRadioButton = findViewById(R.id.radioButton3);
+                RadioButton femaleRadioButton = findViewById(R.id.radioButton2);
+                RadioButton otherRadioButton = findViewById(R.id.radioButton);
+
+
+                if (firstNameEditText.getText().toString().isEmpty() ||
+                        lastNameEditText.getText().toString().isEmpty() ||
+                        birthDateEditText.getText().toString().isEmpty() ||
+                        (!maleRadioButton.isChecked() && !femaleRadioButton.isChecked() && !otherRadioButton.isChecked())) {
+                    Toast.makeText(getApplicationContext(), "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    openProfileCreation2Activity();
+                }
             }
         });
     }
